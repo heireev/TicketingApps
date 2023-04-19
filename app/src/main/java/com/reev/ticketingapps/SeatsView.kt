bf0edc561a608c4f02bb7b65aaec06f7fba61280
heireev <muhammadrifqi216@gmail.com>
2023-04-19 12:13:01 +0800

    [problem] masalah di penomoran kursi

---
+++ app/src/main/java/com/reev/ticketingapps/SeatsView.kt
@@ -0,0 +1,124 @@
+package com.reev.ticketingapps
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+class SeatsView : View {
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    private val seats: ArrayList<Seat> = arrayListOf(
+        Seat(id = 1, name = "A1", isBooked = false),
+        Seat(id = 2, name = "A2", isBooked = false),
+        Seat(id = 3, name = "B1", isBooked = false),
+        Seat(id = 4, name = "A4", isBooked = false),
+        Seat(id = 5, name = "C1", isBooked = false),
+        Seat(id = 6, name = "C2", isBooked = false),
+        Seat(id = 7, name = "D1", isBooked = false),
+        Seat(id = 8, name = "D2", isBooked = false),
+    )
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = getDefaultSize(suggestedMinimumWidth, widthMeasureSpec)
+        val height = getDefaultSize(suggestedMinimumHeight, heightMeasureSpec)
+
+        val halfOfHeight = height / 2
+        val halfOfWidth = width / 2
+        var value = -600F
+
+        for (i in 0..7) {
+            if (i.mod(2) == 0) {
+                seats[i].apply {
+                    x = halfOfWidth - 300F
+                    y = halfOfHeight + value
+                }
+            } else {
+                seats[i].apply {
+                    x = halfOfWidth + 100F
+                    y = halfOfHeight + value
+                }
+                value += 300F
+            }
+        }
+    }
+
+    private val backgroundPaint = Paint()
+    private val armrestPaint = Paint()
+    private val bottomSeatPaint = Paint()
+    private val mBounds = Rect()
+    private val numberSeatPaint = Paint(Paint.FAKE_BOLD_TEXT_FLAG)
+    private val titlePaint = Paint(Paint.FAKE_BOLD_TEXT_FLAG)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        for (seat in seats){
+            drawSeat(canvas, seat)
+        }
+    }
+
+    private fun drawSeat(canvas: Canvas?, seat: Seat) {
+        //Mengatur Warna ketika sudah Dibooking
+        if (seat.isBooked) {
+            backgroundPaint.color = ResourcesCompat.getColor(resources, R.color.grey_200, null)
+            armrestPaint.color = ResourcesCompat.getColor(resources, R.color.grey_200, null)
+            bottomSeatPaint.color = ResourcesCompat.getColor(resources, R.color.grey_200, null)
+            numberSeatPaint.color = ResourcesCompat.getColor(resources, R.color.black, null)
+        } else {
+            backgroundPaint.color = ResourcesCompat.getColor(resources, R.color.blue_500, null)
+            armrestPaint.color = ResourcesCompat.getColor(resources, R.color.blue_700, null)
+            bottomSeatPaint.color = ResourcesCompat.getColor(resources, R.color.blue_200, null)
+            numberSeatPaint.color = ResourcesCompat.getColor(resources, R.color.grey_200, null)
+        }
+
+        //Menyimpan State
+        canvas?.save()
+
+        //Background
+        canvas?.translate(seat.x as Float, seat.y as Float)
+
+        val backgroundPath = Path()
+        backgroundPath.addRect(0F, 0F, 200F, 200F, Path.Direction.CCW)
+        backgroundPath.addCircle(100F, 50F, 75F, Path.Direction.CCW)
+        canvas?.drawPath(backgroundPath, backgroundPaint)
+
+        //Sandaran Tangan
+        val armrestPath = Path()
+        armrestPath.addRect(0F, 0F, 50F, 200F, Path.Direction.CCW)
+        canvas?.drawPath(armrestPath, armrestPaint)
+        canvas?.translate(150F, 0F)
+        armrestPath.addRect(0F, 0F, 50F, 200F, Path.Direction.CCW)
+        canvas?.drawPath(armrestPath,armrestPaint)
+
+        //Bagian Bawah Kursi
+        canvas?.translate(-150F, 175F)
+        val bottomSeatPath = Path()
+        bottomSeatPath.addRect(0F, 0F, 200F, 25F, Path.Direction.CCW)
+        canvas?.drawPath(bottomSeatPath, bottomSeatPaint)
+
+        //Menulis Nomor Kursi
+        canvas?.translate(0F, -175F)
+        numberSeatPaint.apply {
+            textSize = 500F
+            numberSeatPaint.getTextBounds(seat.name, 0, seat.name.length, mBounds)
+        }
+        canvas?.drawText(seat.name, 100F - mBounds.centerX(), 100F, numberSeatPaint)
+
+        //mengembalikan ke pengaturan sebelumnya
+        canvas?.restore()
+    }
+}
